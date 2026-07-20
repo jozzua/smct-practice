def3a2fce23e58b954007eaf91843950cdc5c9a2
Cristian <smct.cristiansimbajon@gmail.com>
2026-07-20 10:28:17 +0800

    Add circular product photos

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,9 @@
-@props(['title' => null])
+@props(['title' => null, 'image' => null, 'imageAlt' => ''])
 
 <div {{ $attributes->merge(['class' => 'card']) }}>
+    @if ($image)
+        <img class="card-product-image" src="{{ $image }}" alt="{{ $imageAlt }}">
+    @endif
     @if ($title)
         <h2 class="card-title">{{ $title }}</h2>
     @endif
